0aaeea7f72406d2c3d1a538d312915b2739ec365
Максим <qwertyhanter21@gmail.com>
2025-12-19 19:31:47 +0200

    Tests: evaluator  базові обчислення, ZeroDivisionError

---
+++ tests/test_evaluator.py
@@ -0,0 +1,17 @@
+﻿from src.parser import ExpressionParser
+from src.evaluator import Evaluator
+
+
+def test_eval_basic():
+    tree = ExpressionParser().parse("2+3*4")
+    result = Evaluator().eval(tree)
+    assert result == 14.0
+
+
+def test_division_by_zero():
+    tree = ExpressionParser().parse("10/0")
+    try:
+        Evaluator().eval(tree)
+        assert False, "Мало бути виключення ZeroDivisionError"
+    except ZeroDivisionError:
+        assert True
