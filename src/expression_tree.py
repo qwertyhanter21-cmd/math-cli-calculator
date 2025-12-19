8056a9ab09a76e0d32fdd8707fbd14053e0a0bd2
Максим <qwertyhanter21@gmail.com>
2025-12-19 19:31:26 +0200

    Parser: структура вузлів дерева виразів (TreeNode)

---
+++ src/expression_tree.py
@@ -0,0 +1,8 @@
+﻿class TreeNode:
+    def __init__(self, value, left=None, right=None):
+        self.value = value
+        self.left = left
+        self.right = right
+
+    def is_leaf(self) -> bool:
+        return self.left is None and self.right is None
